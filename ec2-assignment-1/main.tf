7b7152c32cc31f88bc4b3123ecf1016d44a6c7ba
cauemustafa <caue@42musta.tech>
2024-05-03 18:27:04 -0300

    initial commit to open PR

--- ec2-assignment-1/main.tf
+++ ec2-assignment-1/main.tf
@@ -1,5 +1,5 @@
 #Create the aws provide code. Find help at - https://registry.terraform.io/providers/hashicorp/aws/latest/docs
-provider
+# provider
 
 #Create the EC2 instance resource code. Find help at - https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
-resource
+# resource
