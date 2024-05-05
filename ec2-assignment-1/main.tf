69f722a1f04f686e4d8a0b129c2a372dc91d61ea
cauemustafa <caue@42musta.tech>
2024-05-05 02:01:53 -0300

    create an ec2 instance with a windowns ami

--- ec2-assignment-1/main.tf
+++ ec2-assignment-1/main.tf
@@ -1,5 +1,10 @@
 #Create the aws provide code. Find help at - https://registry.terraform.io/providers/hashicorp/aws/latest/docs
-# provider
+provider "aws" {
+  region = "us-east-2"
+}
 
 #Create the EC2 instance resource code. Find help at - https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
-# resource
+resource "aws_instance" "windows-server-instance" {
+  ami = "ami-0c9890fb99eafa637"
+  instance_type = "t2.micro"
+}
