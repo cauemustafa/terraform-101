032f94023c58937be61dfe3e6004c267b229c1f4
cauemustafa <caue@42musta.tech>
2024-05-05 01:35:40 -0300

    create an EC2 instance

--- ec2-lab-2/main.tf
+++ ec2-lab-2/main.tf
@@ -1,5 +1,9 @@
 provider "aws" {
-    region = "us-east-1"
+    region = "us-east-2"
 }
 
 #Create an EC2 Resource Block
+resource "aws_instance" "example" {
+    ami = "ami-0ddda618e961f2270"
+  instance_type = "t2.micro"
+}
