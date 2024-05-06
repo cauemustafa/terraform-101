ce08a4d1aba73f706322b4184956ba9a002c57fe
cauemustafa <caue@42musta.tech>
2024-05-06 20:26:24 -0300

    lab-4: create a volume and attach to an instance"

--- ec2-lab-4/main.tf
+++ ec2-lab-4/main.tf
@@ -1,19 +1,25 @@
 provider "aws" {
-    region = "YOUR REGION"
+  region = "us-east-1"
 }
 
-resource "aws_instance" "tags-test" {
-    ami = "YOUR AMI"
-    instance_type = "t2.micro"
+resource "aws_instance" "tag-test" {
+  ami           = "ami-07caf09b362be10b8"
+  instance_type = "t2.micro"
+  tags = {
+    Name        = "Tags Test"
+    Environment = "Learning Terraform"
+    Owner       = "42musta"
+    Project     = "Terraform-101 Lab 4"
+  }
 }
 
-# resource "aws_ebs_volume" "example" {
-#   availability_zone = "YOUR AZ"
-#   size              = 40
-# }
+resource "aws_ebs_volume" "example" {
+  availability_zone = "us-east-1b"
+  size              = 40
+}
 
-# resource "aws_volume_attachment" "ebs_att" {
-#   device_name = "/dev/sdh"
-#   volume_id   = 
-#   instance_id = 
-# }
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.example.id
+  instance_id = aws_instance.tag-test.id
+}
