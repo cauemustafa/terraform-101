2acbcbd563f974ca03f260b74ec6a2fc7b179809
cauemustafa <caue@42musta.tech>
2024-05-06 16:33:57 -0300

    create an EC2 instance with tags

--- ec2-assignment-2/main.tf
+++ ec2-assignment-2/main.tf
@@ -1,5 +1,18 @@
 #Create your provider configuration
-
+provider "aws" {
+  region = "us-east-1"
+}
 
 #Create an EC2 Instance with Tags
+resource "aws_instance" "assignment-2" { 
+  ami = "ami-07caf09b362be10b8"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "assigment-2"
+    environment = "learning"
+    purpose = "testing"
+    ami_description = "Amazon Linux 2023 AMI 2023.4.20240429.0 x86_64 HVM kernel-6.1"
+    ami_id = "ami-07caf09b362be10b8"
+  }
+}
 
